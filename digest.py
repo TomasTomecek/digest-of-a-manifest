e39381e90afe1ba0cdda90235810776637bdb4f4
Tomas Tomecek <ttomecek@redhat.com>
2015-10-13 13:15:51 +0200

    fix docs, remove debug prints, create aggresive manifest

--- digest.py
+++ digest.py
@@ -3,10 +3,6 @@
 """
 This script takes at least one argument: path to a file with manifest.
 It then computes and prints digest of the manifest(s).
-The main reason this is not done properly, by decoding json, removing `signatures` and
-encoding again, is that docker messes up encoding of strings and makes it impossible
-to do right. For more info see:
-    https://github.com/docker/distribution/issues/1065
 """
 
 import os
@@ -20,7 +16,11 @@ from collections import OrderedDict
 
 def prepare_file_hack(fd):
     """
-    prepare manifest for generating digest
+    This is a hacky implementation which removes 'signatures' using regular expressions,
+    for more info see:
+
+        https://github.com/docker/distribution/issues/1065
+
     fd: file object returned by open
     """
     # 0 start -> signatures
@@ -50,7 +50,8 @@ def prepare_file_hack(fd):
 
 def prepare_file_decode(fd):
     """
-    prepare manifest for generating digest
+    Remove 'signatures' by deserializing json
+
     fd: file object returned by open
     """
     j = fd.read()
@@ -61,7 +62,7 @@ def prepare_file_decode(fd):
         i = h["v1Compatibility"]
         i = i.replace(r"\u003c", "<").replace(r"\u003e", ">").replace(r"\u0026", "&")
         h["v1Compatibility"] = i
-        print i
+        # print i
 
     encoded_json = json.dumps(decoded_json, indent=3, separators=(',', ': '), ensure_ascii=False)
 
@@ -72,9 +73,13 @@ def prepare_file_decode(fd):
 
 def compute_digest(manifest):
     """
+    E.g.
+
+    sha256:3bd99e8c083e6bb60ef9c1e7de1a2c34a1fab103b6ee4e9c23f08abd91fb6d53
+
     manifest: str
     """
-    return hashlib.sha256(manifest).hexdigest()
+    return "sha256:" + hashlib.sha256(manifest).hexdigest()
 
 
 def main():
@@ -85,7 +90,7 @@ def main():
     for fp in sys.argv[1:]:
         p = os.path.abspath(os.path.expanduser(fp))
         with open(p, "r") as fd:
-            content = prepare_file(fd)
+            content = prepare_file_decode(fd)
             digest = compute_digest(content)
             result.append((fp, digest))
     if len(result) == 1:
--- tests/test_digest.py
+++ tests/test_digest.py
@@ -3,7 +3,7 @@ import os
 from digest import *
 
 
-def test():
+def test_decode_manifest():
     this_dir = os.path.dirname(os.path.realpath(__file__))
     manifest_path = os.path.join(this_dir, "./manifest.json")
     expected_manifest_path = os.path.join(this_dir, "./expected-manifest.json")
@@ -12,12 +12,14 @@ def test():
     result = prepare_file_decode(manifest)
     for idx, v in enumerate(expected_manifest):
         if not v == result[idx]:
-            print repr(expected_manifest[idx-15:idx+45])
-            print repr(result[idx-15:idx+45])
+            # char by char debugging
+            print "-", repr(expected_manifest[idx-15:idx+45])
+            print "-", repr(result[idx-15:idx+45])
             break
     assert result == expected_manifest
 
-def test2():
+
+def test_hack_manifest():
     this_dir = os.path.dirname(os.path.realpath(__file__))
     manifest_path = os.path.join(this_dir, "./manifest.json")
     expected_manifest_path = os.path.join(this_dir, "./expected-manifest.json")
@@ -25,3 +27,13 @@ def test2():
     expected_manifest = open(expected_manifest_path, "r")
     result = prepare_file_hack(manifest)
     assert result == expected_manifest.read()
+
+
+def test_digest():
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    expected_manifest_path = os.path.join(this_dir, "./expected-manifest.json")
+    with open(expected_manifest_path, "r") as fd:
+        expected_manifest = fd.read()
+    digest = compute_digest(expected_manifest)
+    expected_digest = "sha256:3bd99e8c083e6bb60ef9c1e7de1a2c34a1fab103b6ee4e9c23f08abd91fb6d53"
+    assert digest == expected_digest
